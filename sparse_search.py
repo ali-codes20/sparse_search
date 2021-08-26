d8fe810034afa8a6cc9a8558157c014af7afac84
Ali Sadreddin <85346184+ali-codes20@users.noreply.github.com>
2021-08-26 10:13:08 +0100

    Add files via upload

---
+++ sparse_search.py
@@ -0,0 +1,45 @@
+def sparse_search(data, search_val):
+  print("Data: " + str(data))
+  print("Search Value: " + str(search_val))
+  
+  #position in dataset
+  first, last = 0, len(data) - 1
+
+  while first <= last:
+    #calculating middle index using two pointers
+    mid = (first + last) // 2
+    if not data[mid]:
+      left = mid - 1
+      right = mid + 1
+      while True:
+        if left < first and right > last:
+          print(str(search_val) + " is not in the dataset") 
+          return
+        elif right <= last and data[right]:
+          mid = right
+          break
+        elif left >= first and data[left]:
+          mid = left
+          break
+        else:
+          right += 1
+          left -= 1
+    if data[mid] == search_val:
+      print(str(search_val) + " found at position " + str(mid))
+      return
+
+    elif search_val < data[mid]:
+      last = mid - 1
+    elif search_val > data[mid]:
+      first = mid + 1
+  print(str(search_val) + " is not in the dataset")
+
+    
+      
+
+
+        
+
+
+
+  
